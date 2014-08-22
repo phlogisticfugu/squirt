35b936f6a9c7009f77a5a6974afdffd94d6d6022
Phlogistic Fugu <phlogisticfugu@>
2014-08-22 02:21:01 -0700

    using more code-reuse in ArrayCache squirt wrapper class

--- squirt/src/Squirt/Ext/Doctrine/Common/Cache/ArrayCache.php
+++ squirt/src/Squirt/Ext/Doctrine/Common/Cache/ArrayCache.php
@@ -1,26 +1,14 @@
 <?php
 namespace Squirt\Ext\Doctrine\Common\Cache;
 
-use InvalidArgumentException;
 use Doctrine\Common\Cache\ArrayCache as DoctrineArrayCache;
 use Squirt\Common\SquirtableInterface;
-use Squirt\Common\SquirtableTrait;
+use Squirt\Ext\Doctrine\Common\Cache\SquirtableCacheProviderTrait;
 
 /**
  * Provide a Squirt-compatible instance of the Doctrine ArrayCache
  */
 class ArrayCache extends DoctrineArrayCache implements SquirtableInterface
 {
-    use SquirtableTrait;
-    
-    protected function __construct(array $params)
-    {
-        if (array_key_exists('namespace', $params)) {
-            if (is_string($params['namespace'])) {
-                $this->setNamespace($params['namespace']);
-            } else {
-                throw new InvalidArgumentException('namespace must be a string');
-            }
-        }
-    }
+    use SquirtableCacheProviderTrait;
 }
